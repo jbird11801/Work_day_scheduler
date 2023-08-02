4648e9ce6e6be68900db4e0033549ff2e11effd6
jbird11801@gmail.com <jbird11801@gmail.com>
2023-08-01 21:51:30 -0400

    feature/Date

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,6 +2,14 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  // TODO: Add code to display the current date in the header of the page.
+
+  var dateHeaderEl = $('#currentDay');
+
+  var currentDate = dayjs();
+
+  dateHeaderEl.text(currentDate.format('MMM D, YYYY'));
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -18,6 +26,4 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
